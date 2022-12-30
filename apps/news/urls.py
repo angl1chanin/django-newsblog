90cba6109bda3c636819a1ce097fab34cf524efa
slicetopieces <markovich1990mark@gmail.com>
2022-12-30 02:10:13 +0200

    Updated category and article urls view

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from news.views import authors, article, create, category_detail_list
-from news.views import HomeView, ProfileView, ProfileDetailView, CategoryListView
+from news.views import authors, create
+from news.views import HomeView, ProfileView, ProfileDetailView, CategoryListView, CategoryView, ArticleView
 
 app_name = 'news'
 
@@ -10,11 +10,11 @@ urlpatterns = [
     path('create/', create, name='create-article'),
     path('authors/', authors, name='authors-list'),
 
-    path('news/<slug:category>/<slug:article_title>', article, name='article-detail'),
+    path('news/<slug:category>/<slug:article_title>', ArticleView.as_view(), name='article-detail'),
 
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/<str:username>', ProfileDetailView.as_view(), name='profile-detail'),
 
     path('categories/', CategoryListView.as_view(), name='categories-list'),
-    path('news/<slug:category>', category_detail_list, name='category'),
+    path('news/<slug:category>', CategoryView.as_view(), name='category'),
 ]
